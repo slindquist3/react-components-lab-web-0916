c328f4e0fd3482526a2d88d6afbe30f0aca32091
slindquist3 <slindquist3@gmail.com>
2016-11-15 16:24:51 -0500

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -3,10 +3,6 @@ describe('Oldercoaster', function () {
     this.wrapper = enzyme.shallow(React.createElement(OlderCoaster));
   });
 
-  it('should have been created using `React.createClass()`', function () {
-    expect(OlderCoaster.name).toNotExist('The `Oldercoaster` component is being created using a ES2015 class.');
-  });
-
   it('should have the right DOM markup', function () {
     expect(this.wrapper.html()).toEqual(`<div class="oldercoaster"><p>Two grannies having the time of their life!</p><p>Passengers:</p><ul><li>Agnes</li><li>Muriel</li></ul></div>`);
   });
@@ -39,4 +35,3 @@ describe('ButcherShop', function () {
     expect(this.wrapper.html()).toEqual(`<div class="butcher-shop"><p>Hello! We have the following products for sale today:</p><ul><li>Tenderloin</li><li>Short ribs</li><li>Beef shin</li><li>Ribeye</li></ul></div>`);
   });
 });
-
